79b407037dad99640a22e7f9cea656bd9d2859ba
= <=>
2023-07-25 12:12:28 -0400

    Tweaked valve switching timing logic.

--- src/main.rs
+++ src/main.rs
@@ -1,82 +1,97 @@
-use std::{thread, time::{Duration, SystemTime}, io::Read};
-use time::macros::datetime;
-use serialport::{DataBits, StopBits};
-use std::io::{self, Write};
+use std::{thread, time::{Duration, SystemTime}};
+use serialport::{DataBits, StopBits, SerialPort};
+use std::io::{Write};
+
 
 fn main() {
+    let builder = serialport::new("/dev/ttyACM0", 9600) //params to tune for the Arduino port
+        .timeout(Duration::from_millis(3500))
+        .data_bits(DataBits::Eight)
+        .stop_bits(StopBits::One);
+        println!("Waiting for next 5-minute interval...");
 
-    let builder = serialport::new("/dev/ttyACM0", 9600).timeout(Duration::from_millis(3500)).data_bits(DataBits::Eight)
-    .stop_bits(StopBits::One);
+        setup(60*5); //starts it on the next 5mins. Accounts for the time it takes for
+        // the port to reset as well.
 
-    setup(); //starts it on the next 5mins.
+    let mut port = builder.open().expect("failed to connect.");
+    thread::sleep(Duration::from_millis(3000)); //need to wait 3s for the Arduino port to 
+    //reset and communicate. When it's opened here, it will stay open for the lifetime of the
+    //variable "port", which is as long as the loop continues to run. 
 
-    while true {
+    loop {
     //below func gets system time.
     let time_startloop:u64 = SystemTime::now()
-    .duration_since(SystemTime::UNIX_EPOCH).unwrap()
-    .as_secs();
-
-    let mut timing_flags: [i32; 3] = [0,0,0];
-    
-    //second while loop for the interior stuff
-    while timing_flags != [1,1,1] {
-        let time_now:u64 = SystemTime::now()
-        .duration_since(SystemTime::UNIX_EPOCH).unwrap()
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .unwrap()
         .as_secs();
 
-        if time_now - time_startloop == 0 {
-            sendmsg(1);
-            timing_flags[0] = 1;
-            thread::sleep(Duration::from_millis(5*60*1000));
-        }    
+    let mut timing_flags: [i32; 3] = [0,0,0]; //flagging var to check when all 3 levels are cycled thru
+    let routime:u64 = 60*5; //var to define the run time in 1 place
+    let max_buffer:u64 = 2; //maximum error acccepted before a condition is considered skipped
+    
+    //second while loop for the interior stuff
+    while timing_flags != [1,1,1] { //iterate any time the flags aren't all set
+    
+        let time_now:u64 = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap()
+            .as_secs();
 
-        if time_now - time_startloop == 5*60 {
-            sendmsg(2);
+        if time_now - time_startloop >= 0 
+            && time_now-time_startloop <= max_buffer
+            && timing_flags[0]!=1 {
+
+                sendmsg(1, &mut port);
+                timing_flags[0] = 1;
+                println!("Level 1");
+                thread::sleep(Duration::from_secs(routime))
+            }    
+
+        if time_now - time_startloop >= routime 
+        && time_now-time_startloop <= (routime)+max_buffer
+        && timing_flags[1]!=1 {
+
+            sendmsg(2, &mut port);
             timing_flags[1] = 1;
-            thread::sleep(Duration::from_millis(5*60*1000));
+            println!("Level 2");
+            thread::sleep(Duration::from_secs(routime))
         }
         
-        if time_now - time_startloop == 10*60 {
-            sendmsg(0);
-            timing_flags[2] = 1;
-            thread::sleep(Duration::from_millis(5*60*1000));
-        }
+        if time_now - time_startloop >= routime*2
+        && time_now - time_startloop <=(routime*2)+max_buffer
+        && timing_flags[2]!=1 {
 
+            sendmsg(0, &mut port);
+            timing_flags[2] = 1;
+            println!("Level 3");
+            thread::sleep(Duration::from_secs(routime))
+        }
+        
     }
 }
 }
     
-    // match port.read(serial_buf.as_mut_slice()) {
-    //     Ok(t) => io::stdout().write_all(&serial_buf[..t]).unwrap(),
-    //     Err(ref e) if e.kind() == io::ErrorKind::TimedOut => (),
-    //     Err(e) => eprintln!("{:?}", e)}
-
-    // println!("{:?}", serial_buf[0]);
-
-
-
-fn sendmsg(input:u8) {
-
-    let builder = serialport::new("/dev/ttyACM0", 9600).timeout(Duration::from_millis(3500)).data_bits(DataBits::Eight)
-    .stop_bits(StopBits::One);
-
-    let mut port = builder.open().expect("failed to connect.");
-    
+fn sendmsg(input:u8, port: &mut Box<dyn SerialPort>) {   
     let binding: String = input.to_string();
     let writebyte:&[u8] = &binding.as_bytes();
-    thread::sleep(Duration::from_secs(3));
-
-    port.write(writebyte).expect("failed to send!");
+    port
+        .write(writebyte)
+        .expect("failed to send!");
 }
 
-fn setup() {
+fn setup(routime:u64) {
     let mut startflag:u8 = 0;
 
     while startflag == 0 {
-    let time_now:u64 = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap()
-    .as_secs();
-    if time_now % (5*60) == 0 {
+    let time_now:u64 = SystemTime::now()
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .unwrap()
+        .as_secs();
+
+    if time_now % (routime) == 0 {
         startflag = 1;}
         else {startflag = 0;}
     }
 }
+
+
