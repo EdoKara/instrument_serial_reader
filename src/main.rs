1d8bbb59419752351d9bbaec36209997fcfd9ec2
= <=>
2023-07-27 13:28:31 -0400

    modified timing to see about getting better alignment.

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,15 @@ use chrono::{NaiveDateTime, Datelike, Timelike};
 
 const TIME_OFFSET: u64 = 60*11 + 44; //offset between the NOxWerx and the computer this is running on
 const DISPLAYTIME_OFFSET: u64 = 60*60*4; //offset btw EST and UTC (?) 
+
+macro_rules! get_time {
+    () => {SystemTime::now()
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .unwrap()
+        .as_secs() + TIME_OFFSET
+    };
+}
+
 fn main() {
         println!("Waiting for next 5-minute interval...");
         let filepath: &Path = Path::new("C:\\Users\\EIS\\Desktop\\valve_flags");
@@ -17,10 +26,7 @@ fn main() {
 
     loop {
     //below func gets system time.
-    let time_startloop:u64 = SystemTime::now()
-        .duration_since(SystemTime::UNIX_EPOCH)
-        .unwrap()
-        .as_secs() + TIME_OFFSET;
+    let time_startloop:u64 = get_time!();
 
 
     let mut timing_flags: [i32; 3] = [0,0,0]; //flagging var to check when all 3 levels are cycled thru
@@ -30,10 +36,7 @@ fn main() {
     //second while loop for the interior stuff
     while timing_flags != [1,1,1] { //iterate any time the flags aren't all set
 
-        let time_now:u64 = SystemTime::now()
-            .duration_since(SystemTime::UNIX_EPOCH)
-            .unwrap()
-            .as_secs() + TIME_OFFSET;
+        let time_now:u64 = get_time!();
         
         let displaytime = 
         NaiveDateTime::from_timestamp_opt(
@@ -48,14 +51,14 @@ fn main() {
                     Err(_e) => println!("Failed to write!")
                 };
                 timing_flags[0] = 1;
-                println!("Level 1 at {}/{}/{} {}:{}:{}",
+                println!("Level 1 at {:02}/{:02}/{:02} {:02}:{:02}:{:02}",
                 displaytime.year(), displaytime.month(), displaytime.day(),
                 displaytime.hour(), displaytime.minute(), displaytime.second());
                 _ = write_flags(routime, &filepath, &displaytime, 1);
             }    
 
-        if time_now - time_startloop >= routime 
-        && time_now-time_startloop <= (routime)+max_buffer
+        if time_now - time_startloop >= routime - 3
+        && time_now-time_startloop <= (routime - 3 )+max_buffer
         && timing_flags[1]!=1 {
 
             match sendmsg(2){
@@ -63,15 +66,15 @@ fn main() {
                 Err(_e) => println!("Failed to write!")
             };
             timing_flags[1] = 1;
-            println!("Level 2 at {}/{}/{} {}:{}:{}",
+            println!("Level 2 at {:02}/{:02}/{:02} {:02}:{:02}:{:02}",
                 displaytime.year(), displaytime.month(), displaytime.day(),
             displaytime.hour(), displaytime.minute(), displaytime.second());
 
             _ = write_flags(routime, &filepath, &displaytime, 2);
         }
         
-        if time_now - time_startloop >= routime*2
-        && time_now - time_startloop <=(routime*2)+max_buffer
+        if time_now - time_startloop >= routime*2 - 3 
+        && time_now - time_startloop <=(routime*2 - 3 )+max_buffer
         && timing_flags[2]!=1 {
 
             match sendmsg(0){
@@ -79,7 +82,7 @@ fn main() {
                 Err(_e) => println!("Failed to write!")
             };
             timing_flags[2] = 1;
-            println!("Level 3 at {}/{}/{} {}:{}:{}",
+            println!("Level 3 at {:02}/{:02}/{:02} {:02}:{:02}:{:02}",
                 displaytime.year(), displaytime.month(), displaytime.day(),
             displaytime.hour(), displaytime.minute(), displaytime.second());
             _ = write_flags(routime, &filepath, &displaytime, 3);
@@ -87,7 +90,7 @@ fn main() {
 
         if time_now - time_startloop >= routime*3 + 15 &&
         timing_flags !=[1,1,1]{
-            println!("Overtime at {}/{}/{} {}:{}:{}",
+            println!("Overtime at {:02}/{:02}/{:02} {:02}:{:02}:{:02}",
                 displaytime.year(), displaytime.month(), displaytime.day(),
             displaytime.hour(), displaytime.minute(), displaytime.second());
             sendmsg(0).expect("failed!");
@@ -136,20 +139,31 @@ fn write_flags(routime:u64,
     displaytime:&NaiveDateTime, 
     position:u8)-> Result<()> {
 
-    let displaystring = format!("{}{}{}{}{}", //format YMDHM
+    let displaystring = format!("{:02}{:02}{:02}{:02}{:02}", //format YYMMDDHHMM
         displaytime.year(), 
         displaytime.month(),displaytime.day(),
         displaytime.hour(),displaytime.minute()
         );
+
     let filenamestr: String = format!("rob_noxbox_{}.csv",&displaystring); //concatenating the filenamestr
+
     let file = filepath.join(&filenamestr); //final filename
+
     let mut wtr = csv::Writer::from_path(file)?; //pass it to the writer function
 
     let mut counter: u64 = 0; 
+    wtr.write_record(vec!["datetime","position"])?; //the header of the file.
 
     while counter < routime { //write 1 entry per second with the flags.
-        wtr.write_record(vec![&displaytime.to_string(), &position.to_string()])?;
+
+        let now = get_time!();
+        let displaynow = NaiveDateTime::from_timestamp_opt(
+            (now - DISPLAYTIME_OFFSET) as i64,0_u32)
+            .unwrap();
+
+        wtr.write_record(vec![displaynow.to_string(), position.to_string()])?;
         counter += 1;
+
         thread::sleep(Duration::from_millis(1000))
     }   
     wtr.flush()?;
