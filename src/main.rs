52cc0be655728c6eb54b41e3f51ce9325ae1c912
= <=>
2023-07-27 12:24:42 -0400

    Added flagging func.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
-use std::{thread, time::{Duration, SystemTime}, io::Result};
+use std::{thread, 
+    time::{Duration, SystemTime}, 
+    io::Result, env, error::Error,
+    ffi::OsString,process, path::{Path, self}};
 use serialport::{DataBits, StopBits};
 use chrono::{NaiveDateTime, Datelike, Timelike};
 
 
-const TIME_OFFSET: u64 = 60*11 + 44;
-
+const TIME_OFFSET: u64 = 60*11 + 44; //offset between the NOxWerx and the computer this is running on
+const DISPLAYTIME_OFFSET: u64 = 60*60*4; //offset btw EST and UTC (?) 
 fn main() {
-        println!("Waiting for next 15-minute interval...");
+        println!("Waiting for next 5-minute interval...");
+        let filepath: &Path = Path::new("C:\\Users\\EIS\\Desktop\\valve_flags");
+        
         setup(5*60); //starts it on the next 5mins. Accounts for the time it takes for
         // the port to reset as well.
+
     loop {
     //below func gets system time.
     let time_startloop:u64 = SystemTime::now()
@@ -29,7 +35,10 @@ fn main() {
             .unwrap()
             .as_secs() + TIME_OFFSET;
         
-        let displaytime = NaiveDateTime::from_timestamp_opt(time_now as i64,1_u32).unwrap();
+        let displaytime = 
+        NaiveDateTime::from_timestamp_opt(
+            (time_now - DISPLAYTIME_OFFSET) as i64,0_u32)
+        .unwrap();
 
         if  time_now-time_startloop <= max_buffer
             && timing_flags[0]!=1 {
@@ -41,9 +50,8 @@ fn main() {
                 timing_flags[0] = 1;
                 println!("Level 1 at {}/{}/{} {}:{}:{}",
                 displaytime.year(), displaytime.month(), displaytime.day(),
-            displaytime.hour(), displaytime.minute(), displaytime.second());
-                thread::sleep(Duration::from_secs(routime)
-            );
+                displaytime.hour(), displaytime.minute(), displaytime.second());
+                _ = write_flags(routime, &filepath, &displaytime, 1);
             }    
 
         if time_now - time_startloop >= routime 
@@ -59,7 +67,7 @@ fn main() {
                 displaytime.year(), displaytime.month(), displaytime.day(),
             displaytime.hour(), displaytime.minute(), displaytime.second());
 
-            thread::sleep(Duration::from_secs(routime));
+            _ = write_flags(routime, &filepath, &displaytime, 2);
         }
         
         if time_now - time_startloop >= routime*2
@@ -74,7 +82,7 @@ fn main() {
             println!("Level 3 at {}/{}/{} {}:{}:{}",
                 displaytime.year(), displaytime.month(), displaytime.day(),
             displaytime.hour(), displaytime.minute(), displaytime.second());
-            thread::sleep(Duration::from_secs(routime));
+            _ = write_flags(routime, &filepath, &displaytime, 3);
         }
 
         if time_now - time_startloop >= routime*3 + 15 &&
@@ -121,5 +129,58 @@ fn setup(routime:u64) {
         startflag = 1;}
         else {startflag = 0;}
     }
+}
+
+fn write_flags(routime:u64, 
+    filepath:&Path, 
+    displaytime:&NaiveDateTime, 
+    position:u8)-> Result<()> {
+
+    let displaystring = format!("{}{}{}{}{}", //format YMDHM
+        displaytime.year(), 
+        displaytime.month(),displaytime.day(),
+        displaytime.hour(),displaytime.minute()
+        );
+    let filenamestr: String = format!("rob_noxbox_{}.csv",&displaystring); //concatenating the filenamestr
+    let file = filepath.join(&filenamestr); //final filename
+    let mut wtr = csv::Writer::from_path(file)?; //pass it to the writer function
+
+    let mut counter: u64 = 0; 
+
+    while counter < routime { //write 1 entry per second with the flags.
+        wtr.write_record(vec![&displaytime.to_string(), &position.to_string()])?;
+        counter += 1;
+        thread::sleep(Duration::from_millis(1000))
+    }   
+    wtr.flush()?;
+    Ok(())
+}
+
+
+#[test]
+//time test
+fn filename() {
+    let time_now:u64 = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap()
+            .as_secs() + TIME_OFFSET;
+
+    let displaytime = 
+        NaiveDateTime::from_timestamp_opt(
+            (time_now - DISPLAYTIME_OFFSET) as i64,0_u32)
+        .unwrap();
+
+    let displaystr = format!("{}{}{}{}{}",
+        displaytime.year(),displaytime.month(),
+        displaytime.day(),displaytime.hour(),
+        displaytime.minute());
+
+        let filepath: &Path = Path::new("C:\\Users\\EIS\\Desktop\\valve_flags");
+        let filenamestr: String = format!("rob_noxbox_{}.csv", &displaytime.to_string());
+        let file = filepath.join(&filenamestr);
+
+        println!("{}",file.display());
+        debug_assert!()
+
 }
 
